3eb40ef5945af6c6d7384ebd5c8e199a530e23f8
cristf1 <cristf1@users.noreply.github.com>
2023-01-31 15:11:11 -0800

    updated 3.4: connect API to code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movieData, onMovieClick }) => {
     return (
         <div
@@ -8,4 +10,22 @@ export const MovieCard = ({ movieData, onMovieClick }) => {
         </div>
     );
 };
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired
+      }),
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired,
+        Birth: PropTypes.number.isRequired,
+      }),
+    }).isRequired,
+  
+    onMovieClick: PropTypes.func.isRequired
+  };
 
